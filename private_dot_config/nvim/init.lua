79b132a5d0999792d62c2beb2797bc2e01238287
bryanhuhta <bryan.huhta@grafana.com>
2026-03-19 15:50:05 -0700

    More modifications to nvim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -18,6 +18,24 @@ require("lazy").setup({
     ft = { "go" },
   },
   {
+    "ibhagwan/fzf-lua",
+    config = function()
+      local fzf = require("fzf-lua")
+      fzf.setup({
+        winopts = {
+          on_create = function()
+            vim.keymap.set("t", "<C-r>", function()
+              return '<C-\\><C-N>"' .. vim.fn.nr2char(vim.fn.getchar()) .. "pi"
+            end, { expr = true, buffer = true })
+          end,
+        },
+      })
+      vim.keymap.set("n", "<leader>f", fzf.files, { silent = true, desc = "Fuzzy find files" })
+      vim.keymap.set("n", "<leader>b", fzf.buffers, { silent = true, desc = "Fuzzy find buffers" })
+    end,
+  },
+  { "tpope/vim-fugitive" },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
@@ -57,6 +75,10 @@ vim.opt.foldlevel = 99
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Search with ag (Silver Searcher) via :grep, results in quickfix list
+vim.opt.grepprg = "ag --vimgrep"
+vim.opt.grepformat = "%f:%l:%c:%m"
+
 -- Keymaps
 vim.keymap.set("n", "<C-E>", ":Lexplore<CR>", { silent = true, desc = "Toggle file explorer" })
 vim.keymap.set("n", "<leader>r", ":set relativenumber! number!<CR>", { desc = "Toggle relative/absolute line numbers" })
@@ -64,6 +86,8 @@ vim.keymap.set("n", "<Leader>w", ":%s/\\s\\+$//e<CR>", { desc = "Trim trailing w
 vim.keymap.set("v", "<Leader>W", "<Cmd>set textwidth=80<CR>gvgq", { desc = "Hard wrap selection to 80 columns" })
 vim.keymap.set("n", "<Leader>z", ":set spell!<CR>", { desc = "Toggle spell checking" })
 vim.keymap.set("n", "<C-j>", "i<CR><CR><Up><C-t>", { desc = "Insert newline with indentation (e.g. expand braces)" })
+vim.keymap.set("n", "<leader>q", ":copen<CR>", { silent = true, desc = "Open quickfix list" })
+vim.keymap.set("v", "//", [[y/\V<C-R>=escape(@", '/\')<CR><CR>]], { desc = "Search for visual selection" })
 
 -- vim-go
 vim.g.go_fmt_command = "goimports"
