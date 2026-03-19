79aa75c8aa9e045ed1329ba17ca66df01fb8be71
bryanhuhta <bryan.huhta@grafana.com>
2026-03-19 11:10:08 -0700

    Add nvim configs

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -21,9 +21,8 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter").setup({
         ensure_installed = { "typescript", "tsx", "go", "lua" },
-        highlight = { enable = true },
       })
     end,
   },
