6c5cdf70ddb47089e47448c9b058c5039fc52628
bryanhuhta <bryan.huhta@grafana.com>
2026-03-24 10:04:51 -0700

    Add typescript lsp

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -52,6 +52,33 @@ require("lazy").setup({
   },
 })
 
+-- LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local buf = ev.buf
+    local function map(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = buf, desc = desc })
+    end
+    map("n", "gd",         vim.lsp.buf.definition,     "Go to definition")
+    map("n", "gD",         vim.lsp.buf.declaration,    "Go to declaration")
+    map("n", "gr",         vim.lsp.buf.references,     "Go to references")
+    map("n", "gi",         vim.lsp.buf.implementation, "Go to implementation")
+    map("n", "K",          vim.lsp.buf.hover,          "Hover documentation")
+    map("n", "<leader>rn", vim.lsp.buf.rename,         "Rename symbol")
+    map("n", "<leader>ca", vim.lsp.buf.code_action,    "Code action")
+    map("n", "<leader>e",  vim.diagnostic.open_float,  "Show line diagnostics")
+    map("n", "[d",         vim.diagnostic.goto_prev,   "Previous diagnostic")
+    map("n", "]d",         vim.diagnostic.goto_next,   "Next diagnostic")
+  end,
+})
+
+vim.lsp.config("ts_ls", {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+})
+vim.lsp.enable("ts_ls")
+
 -- General
 vim.opt.ruler = true
 vim.opt.relativenumber = true
